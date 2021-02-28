0ea660a1b9b2aed15bb12e358bb568a8d7be4abe
Zhicheng <chenzhicheng1994@gmail.com>
2021-02-28 12:16:57 +0800

    feat: support create->save->read pub/priv key cross test

--- implement/sm2_impl/sm2_test.go
+++ implement/sm2_impl/sm2_test.go
@@ -22,57 +22,75 @@ func initImplB() sm2_impl.Sm2Creator {
 // TestCreateKeyAndSavePubKeyPem tests CreateKey() then
 //   save pubKey to pem and read it.
 func TestCreateKeyAndSavePubKeyPem(t *testing.T) {
-	keyCreatorA := initImplA()
-	pubKeyA := keyCreatorA.CreateKey().PublicKey()
-	assert.NotNil(t, pubKeyA, "init failed")
-	keyCreatorB := initImplB()
-	pubKeyB := keyCreatorB.CreateKey().PublicKey()
-	assert.NotNil(t, pubKeyB, "init failed")
+	testParameters := []struct {
+		alice sm2_impl.Sm2Creator
+		bob   sm2_impl.Sm2Creator
+	}{
+		{initImplA(), initImplB()},
+		{initImplB(), initImplA()},
+	}
+	for _, parameter := range testParameters {
+		keyCreatorA := parameter.alice
+		pubKeyA := keyCreatorA.CreateKey().PublicKey()
+		assert.NotNil(t, pubKeyA, "init failed")
+		keyCreatorB := parameter.bob
+		pubKeyB := keyCreatorB.CreateKey().PublicKey()
+		assert.NotNil(t, pubKeyB, "init failed")
 
-	// pub key A -> pem -> pub key B
-	pubKeyPemA, err := pubKeyA.WriteToPem()
-	assert.Nil(t, err, "pub key A write to pem failed")
-	pubKeyA2B, err := pubKeyB.ReadFromPem(pubKeyPemA)
-	assert.Nil(t, err, "read pem from pub key A failed")
+		// pub key A -> pem -> pub key B
+		pubKeyPemA, err := pubKeyA.WriteToPem()
+		assert.Nil(t, err, "pub key A write to pem failed")
+		pubKeyA2B, err := pubKeyB.ReadFromPem(pubKeyPemA)
+		assert.Nil(t, err, "read pem from pub key A failed")
 
-	// pub key B -> pem -> new pub key A
-	pubKeyPemA2B, err := pubKeyA2B.WriteToPem()
-	assert.Nil(t, err, "pub key B write to pem failed")
-	pubKeyA2B2A, err := pubKeyA.ReadFromPem(pubKeyPemA2B)
-	assert.Nil(t, err, "pub key A read from B failed")
+		// pub key B -> pem -> new pub key A
+		pubKeyPemA2B, err := pubKeyA2B.WriteToPem()
+		assert.Nil(t, err, "pub key B write to pem failed")
+		pubKeyA2B2A, err := pubKeyA.ReadFromPem(pubKeyPemA2B)
+		assert.Nil(t, err, "pub key A read from B failed")
 
-	// compare new pub key A with origin
-	pubKeyPemA2B2A, err := pubKeyA2B2A.WriteToPem()
-	assert.Nil(t, err, "pub key A write to pem failed")
-	assert.Equal(t, pubKeyPemA, pubKeyPemA2B2A, "new pub key A should equal the origin one")
+		// compare new pub key A with origin
+		pubKeyPemA2B2A, err := pubKeyA2B2A.WriteToPem()
+		assert.Nil(t, err, "pub key A write to pem failed")
+		assert.Equal(t, pubKeyPemA, pubKeyPemA2B2A, "new pub key A should equal the origin one")
+	}
 }
 
 // TestCreateKeyAndSavePrivKeyPem tests CreateKey() then
 //   save privKey to pem and read it.
 func TestCreateKeyAndSavePrivKeyPem(t *testing.T) {
-	keyCreatorA := initImplA()
-	privKeyA := keyCreatorA.CreateKey()
-	assert.NotNil(t, privKeyA, "init failed")
-	keyCreatorB := initImplB()
-	privKeyB := keyCreatorB.CreateKey()
-	assert.NotNil(t, privKeyB, "init failed")
+	testParameters := []struct {
+		alice sm2_impl.Sm2Creator
+		bob   sm2_impl.Sm2Creator
+	}{
+		{initImplA(), initImplB()},
+		{initImplB(), initImplA()},
+	}
+	for _, parameter := range testParameters {
+		keyCreatorA := parameter.alice
+		privKeyA := keyCreatorA.CreateKey()
+		assert.NotNil(t, privKeyA, "init failed")
+		keyCreatorB := parameter.bob
+		privKeyB := keyCreatorB.CreateKey()
+		assert.NotNil(t, privKeyB, "init failed")
 
-	// priv key A -> pem -> priv key B
-	privKeyPemA, err := privKeyA.WriteToPem()
-	assert.Nil(t, err, "priv key A write to pem failed")
-	privKeyA2B, err := privKeyB.ReadFromPem(privKeyPemA)
-	assert.Nil(t, err, "read pem from priv key A failed")
+		// priv key A -> pem -> priv key B
+		privKeyPemA, err := privKeyA.WriteToPem()
+		assert.Nil(t, err, "priv key A write to pem failed")
+		privKeyA2B, err := privKeyB.ReadFromPem(privKeyPemA)
+		assert.Nil(t, err, "read pem from priv key A failed")
 
-	// priv key B -> pem -> new priv key A
-	privKeyPemA2B, err := privKeyA2B.WriteToPem()
-	assert.Nil(t, err, "priv key B write to pem failed")
-	privKeyA2B2A, err := privKeyA.ReadFromPem(privKeyPemA2B)
-	assert.Nil(t, err, "priv key A read from B failed")
+		// priv key B -> pem -> new priv key A
+		privKeyPemA2B, err := privKeyA2B.WriteToPem()
+		assert.Nil(t, err, "priv key B write to pem failed")
+		privKeyA2B2A, err := privKeyA.ReadFromPem(privKeyPemA2B)
+		assert.Nil(t, err, "priv key A read from B failed")
 
-	// compare new priv key A with origin
-	privKeyPemA2B2A, err := privKeyA2B2A.WriteToPem()
-	assert.Nil(t, err, "new priv key A write to pem failed")
-	assert.Equal(t, privKeyPemA, privKeyPemA2B2A, "new priv key A should equal the origin one")
+		// compare new priv key A with origin
+		privKeyPemA2B2A, err := privKeyA2B2A.WriteToPem()
+		assert.Nil(t, err, "new priv key A write to pem failed")
+		assert.Equal(t, privKeyPemA, privKeyPemA2B2A, "new priv key A should equal the origin one")
+	}
 }
 
 // TestEncryptAndDecryptCompatibility tests compatibility between Alice and Bob
